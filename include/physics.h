a9066926f28e89f9abee8c9f1b50463d2fe20cd9
rfloresx <rflores4580@gmail.com>
2018-12-01 21:25:30 -0600

    Add Collider component

--- include/physics.h
+++ include/physics.h
@@ -21,6 +21,18 @@ typedef struct EcsAngularSpeed {
 
 typedef EcsVector3D EcsAngularVelocity;
 
+typedef EcsVector4D EcsAABB;
+typedef EcsVector2D EcsPoint2D;
+
+typedef struct EcsCircle2DCollider {
+    float radius;
+} EcsCircleCollider;
+
+typedef struct EcsPolygon2DCollider {
+    EcsPoint2D *points;
+    int8_t point_count; //MAX 128
+} EcsPolygonCollider;
+
 typedef struct EcsComponentsPhysicsHandles {
     EcsHandle Speed;
     EcsHandle Velocity2D;
